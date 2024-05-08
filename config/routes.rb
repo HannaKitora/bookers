8c5310aa39d9458282ee62988e67e233a66d5c0a
HannaKitora <h.wimper@gmail.com>
2024-05-08 12:28:16 +0000

    202405082128

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   get 'books/new'
+  get 'homes/top' => 'homes#top', as: 'top'
   post 'books' => 'books#create'
-  get 'books' => 'books#index'
-  get 'books/:id' => 'books#show'
-  get 'books/edit'
-  get 'homes/top' => 'homes#top'
+  get 'books' => 'books#index' => 'index_book'
+  get 'books/:id' => 'books#show', as: 'book'
+  get 'books/edit' => 'books#edit', as: 'edit_book'
+  patch 'book/:id' => 'books#update', as: 'update_book'
+  
+  delete 'books/destroy' => 'books#destroy', as: 'book_destroy'
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
