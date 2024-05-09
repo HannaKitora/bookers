9298cb10069eb6972aec112130e31f48ffbc4d1f
HannaKitora <h.wimper@gmail.com>
2024-05-09 14:54:36 +0000

    [20240509]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,18 +1,20 @@
 class BooksController < ApplicationController
+ 
   def new
   end
-
-  
+ 
   def create
-    book = Book.new(book_params)
+    @book = Book.new(book_params)
+    
     if @book.save
       redirect_to book_path(@book),notice: "Book was successfully created."
     else
       render :new
     end
   end
-
   
+  
+
   def index
     @books = Book.all
     
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  get 'start', to: 'books#start'
-  get 'homes/top' => 'homes#top', as: 'top'
-  post 'books' => 'books#create' => 'create_book'
+  get '/top' => 'homes#top', as: 'top'
+  get 'start' => 'books#start'
+  post 'books' => 'books#create'
   get 'books' => 'books#index', as: 'index_book'
   get 'books/:id' => 'books#show', as: 'show_book'
-  get 'books/edit' => 'books#edit', as: 'edit_book'
-  patch 'update_book/:id' => 'books#update', as: 'update_book'
+  get 'books/new'
+  #get 'book/new' => 'book#new', as: 'new_book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  #get 'books/back' => 'index_book'
+  patch 'books/:id' => 'books#update', as: 'update_book'
   
-  delete 'books/destroy' => 'books#destroy', as: 'destroy_book'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
